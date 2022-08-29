398605faebb220af92bbd615f8b2a5a0ac447ca1
UsameePhumswang <88129589+UsameePhumswang@users.noreply.github.com>
2022-08-29 20:21:27 +0700

    Update fillStartWord.js

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,5 +2,12 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
+  if(word.startsWith(startWord)){
+    return word
+  } else if(word === null || word === undefiend){
+    return undefined
+  }else {
+    return startWith + word
+  }
 }
 module.exports = fillStartWord
